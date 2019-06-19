4c8056f6f29ae666f30679e9688162cfce6466c9
Nicolas Thomas <n.thomas@waigeo.fr>
2019-06-19 17:09:47 +0200

    fix samsung 5

--- src/android/API26Wrapper.java
+++ src/android/API26Wrapper.java
@@ -32,7 +32,7 @@ public class API26Wrapper {
     /**
      * The identifier for the notification displayed for the foreground service.
      */
-    private static final int NOTIFICATION_ID = 12345678;
+    public static final int NOTIFICATION_ID = 12345678;
 
 
     public final static String NOTIFICATION_CHANNEL_ID = "Notification";
@@ -40,7 +40,8 @@ public class API26Wrapper {
     public static void startForegroundService(final Context context, final Intent intent) {
         context.startForegroundService(intent);
         //We only need to call this for SDK 26+, since startForeground always has to be called after startForegroundService.
-        startForeground(NOTIFICATION_ID, getNotificationBuilder(context).build()); //getNotification, try to fix bug on samsung
+        Notification notification = getNotificationBuilder().build();
+        startForeground(NOTIFICATION_ID, notification); //getNotification, try to fix bug on samsung
     }
 
     // error on samsung note 9 > api 28
@@ -53,8 +54,8 @@ public class API26Wrapper {
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationChannel channel =
                 new NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_ID,
-                        NotificationManager.IMPORTANCE_NONE);
-        channel.setImportance(NotificationManager.IMPORTANCE_MIN); // ignored by Android O ... => IMPORTANCE_LOW
+                        NotificationManager.IMPORTANCE_LOW); //IMPORTANCE_NONE
+        //channel.setImportance(NotificationManager.IMPORTANCE_MIN); // ignored by Android O ... => IMPORTANCE_LOW
         channel.enableLights(false);
         channel.enableVibration(false);
         channel.setBypassDnd(false);
