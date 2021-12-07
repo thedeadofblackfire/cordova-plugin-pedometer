c275f535ceecb2738613f9a2708ba8f22f8d6a81
Nicolas Thomas <thedeadofblackfire@yahoo.com>
2021-12-07 15:43:47 +0100

    minor changes on db

--- src/android/Database.java
+++ src/android/Database.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2013 Thomas Hoffmann
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.apache.cordova.pedometer;
 
 import android.content.ContentValues;
@@ -795,7 +779,14 @@ public class Database extends SQLiteOpenHelper {
 
         return returnObj;
     }
-
+	
+    /**
+     * Clean all data
+     */
+    public void clear() {
+        getWritableDatabase().execSQL("delete from " + TABLE_STEPS + "");
+    }
+	
     /**
      * Clean all synced data
      */
@@ -820,7 +811,7 @@ public class Database extends SQLiteOpenHelper {
     }
 
     /**
-     * Rollback  the lines to sync for error
+     * Rollback the lines to sync for error
      */
     public void rollbackLinesToSync() {
         getWritableDatabase().execSQL(
